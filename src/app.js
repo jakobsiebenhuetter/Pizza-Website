84bf9d6fdd8aa2fab2fe6e843906a249fe300862
Jakob Siebenhuetter <jakob.siebenhuetter@gmx.at>
2025-09-06 21:17:39 +0200

    project finished

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const path = require('path');
 const fs = require('fs');
 const multer = require('multer');
 
-
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
@@ -23,8 +22,9 @@ app.use(express.urlencoded( { extended: true }))
     })
 
 const upload = multer( { storage: storage} )
-const limit = 3;
+const limit = 6;
 const filePath = path.join(__dirname, '../data.json');
+
 
 app.get('/', (req, res) => {
   let data = [];
@@ -32,11 +32,18 @@ app.get('/', (req, res) => {
   pizzaData = JSON.parse(pizzaData);
 
   data = pizzaData;
-  let pizzaPages = data.slice(0, 3);
-  let pagInfo = 2;
+  let pizzaPages = data.slice(0, 6);
+
   let maxPages = Math.ceil(data.length / limit);
-  res.render('index', { pizzaPages: pizzaPages, maxPages: maxPages, pagInfo: pagInfo  });
+  res.render('index', { pizzaPages: pizzaPages, maxPages: maxPages, page: 1 });
 });
+
+app.get('/index', (req, res) => {
+
+  res.redirect('/');
+});
+
+
 
 app.get('/index/:id', (req, res) => {
     let page = parseInt(req.params.id);
@@ -45,18 +52,38 @@ app.get('/index/:id', (req, res) => {
 
     pizzaData = JSON.parse(pizzaData);
     data = pizzaData;
-    let pagInfo = null;
-    if(page === Math.ceil(data.length / limit)) {
-      pagInfo = 1;
-    } else if (page === 1) {
-      pagInfo = 2;
-    }
-console.log(pagInfo)
+
     let maxPages = Math.ceil(data.length / limit);
     let startIndex = (page - 1) * limit;
     let endIndex = page * limit;   
     let pizzaPages = data.slice(startIndex, endIndex);
-  res.render('index', {pizzaPages: pizzaPages, page: page, maxPages: maxPages, pagInfo: pagInfo });
+    
+    res.render('index', {pizzaPages: pizzaPages, page: page, maxPages: maxPages});
+});
+
+app.post('/search', (req, res) => {
+  const val = req.body.search;
+  let startIndex = 0;
+  let endIndex = 6;
+  let data = [];
+  let filteredData = [];
+  let pizzaData = fs.readFileSync(filePath) || null;
+  pizzaData = JSON.parse(pizzaData);
+  data = pizzaData;
+
+  let i = 0;
+  while(i < data.length - 1) {
+    if(data[i].name.indexOf(val) !== -1 || data[i].description.indexOf(val) !== -1) {
+      filteredData.push(data[i]);
+    };
+    i++;
+  };
+
+
+    let pizzaPages = filteredData.slice(startIndex, endIndex);
+
+
+  res.render(`index`, { pizzaPages: pizzaPages, page: null, maxPages: 0 } )
 });
 
 app.get('/addPizza', (req, res)=> {
@@ -66,18 +93,14 @@ app.get('/addPizza', (req, res)=> {
 app.post('/savePizza', upload.single('image'), (req, res) => {
    const body = req.body;
    const file = req.file;
-   console.log(file);
-  //  if(!file) {
-  //   return res.json('Test');
-  //  };
-    const pizzaData = {
+   if(!body.name || !body.description || !file) {
+    return res.redirect('/index');
+   }
+   const pizzaData = {
     name: body.name,
     description: body.description,
     filePath: file ? file.filename : null
   }
-
-
-  // try {
 
     let fileData = fs.readFileSync(path.join(__dirname, '../data.json'), { encoding: 'utf8'}) || [];
     fileData = JSON.parse(fileData);
@@ -85,14 +108,13 @@ app.post('/savePizza', upload.single('image'), (req, res) => {
     fileData.push(pizzaData);
     
     fs.writeFileSync(path.join(__dirname, '../data.json'), JSON.stringify(fileData))
-  // } catch (error) {
-  //   console.error(error);
-  // };
-  
-
-
-   res.redirect('/addPizza');
+    
+    res.redirect('/addPizza');
 });
+
+function search(value) {
+
+}
 
 const port = 3000;
 app.listen(port, () => { console.log(`Webserver listen on Port: ${port}`)});
